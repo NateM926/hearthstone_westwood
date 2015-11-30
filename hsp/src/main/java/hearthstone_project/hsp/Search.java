b68b86a451811cc0e2a62b9c573e0a5b5978b350
Max Geffken <maximilian.geffken@maine.edu>
2015-11-29 21:03:35 -0500

    bugfix and some solid search method skeletons

--- hsp/src/main/java/hearthstone_project/hsp/Search.java
+++ hsp/src/main/java/hearthstone_project/hsp/Search.java
@@ -1,9 +1,68 @@
 package hearthstone_project.hsp;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+
 public class Search {
 
-	public Search() {
+	public Search() 
+	{
 		System.out.println("");
-		
 	}
+	
+	public HttpResponse<JsonNode> SearchAll()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchBack()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchName()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchSet()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchClass()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchFaction()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchQuality()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchRace()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchType()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchInfo()
+	{
+		return null;
+	}
+	
+	public HttpResponse<JsonNode> SearchCard()
+	{
+		return null;
+	}
+	
 }
